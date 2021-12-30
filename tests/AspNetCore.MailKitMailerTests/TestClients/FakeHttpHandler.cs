b15a06f26f42c1b7b9a79a14c4ca26d9796a5127
Malte <malte@networkfreaks.de>
2021-12-30 15:51:06 +0100

    Removed Obsolete WebClient and fixed attachment builder

---
+++ tests/AspNetCore.MailKitMailerTests/TestClients/FakeHttpHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCore.MailKitMailerTests.TestClients
+{
+
+    public class FakeHttpHandler : HttpMessageHandler
+    {
+        public virtual HttpResponseMessage Send(HttpRequestMessage request)
+        {
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Send(request));
+        }
+    }
+}
